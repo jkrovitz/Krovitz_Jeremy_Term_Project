821d86bd6bfca1d1f32af46e859c0fd91dc28d79
Jeremy Krovitz <jkrovitz@Jeremy-William-Comp.local>
2021-06-13 19:21:00 -0500

    Add registration form

---
+++ js/register.js
@@ -0,0 +1,91 @@
+(function () {
+	'use strict';
+	document.registrationForm.noValidate = true;
+	const USERNAME = document.getElementById('username');
+	const PASSWORD = document.getElementById('password');
+	const EMAIL = document.getElementById('email');
+	const SUBMIT = document.getElementById('submit');
+	SUBMIT.disabled = true;
+
+	[USERNAME, PASSWORD, EMAIL].forEach(item => {
+		item.addEventListener('input', function () {
+			let usernameValid = validateUsername(USERNAME.value, document.getElementsByTagName('p')[1]);
+			let emailValid = validateEmail(EMAIL.value, document.getElementsByTagName('p')[2]);
+			let passwordValid = validatePassword(PASSWORD.value, document.getElementsByTagName('p')[3]);
+			 if ((usernameValid === true) && (emailValid === true) && (passwordValid === true)) {
+                SUBMIT.disabled = false;
+                SUBMIT.removeAttribute('class', 'disabledSubmit');
+                SUBMIT.setAttribute('class', 'enabledSubmit');
+            } else {
+                SUBMIT.disabled = true;
+                SUBMIT.setAttribute('class', 'disabledSubmit');
+                SUBMIT.removeAttribute('class', 'enabledSubmit');
+            }
+		}, false);
+	});
+
+	function validateUsername(usernameValue, usernameFeedback) {
+        let usernameValid;
+        const USERNAME_REGEX = /^[A-Za-z0-9_-]{5,20}$/;
+        if (!(USERNAME_REGEX.test(usernameValue))) {
+            usernameFeedback.classList.remove('hidden');
+            usernameValid = false;
+        } else {
+            usernameFeedback.setAttribute('class', 'hidden');
+            usernameValid = true;
+        }
+        return usernameValid;
+	}
+
+	function validateEmail(emailValue, emailFeedback) {
+        let emailValid;
+        const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!(EMAIL_REGEX.test(emailValue))) {
+            emailFeedback.classList.remove('hidden');
+            emailValid = false;
+        } else {
+            emailFeedback.setAttribute('class', 'hidden');
+            emailValid = true;
+        }
+        return emailValid;
+	}
+
+// 	function validatePassword(passwordValue, passwordFeedback) {
+//         let passwordValid;
+//         const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/;
+//         if (!(PASSWORD_REGEX.test(passwordValue))) {
+//             passwordFeedback.classList.remove('hidden');
+//             passwordValid = false;
+//         } else {
+//             passwordFeedback.setAttribute('class', 'hidden');
+//             passwordValid = true;
+//         }
+//         return passwordValid;
+// 	}
+// }());
+
+	function validatePassword(passwordValue, passwordFeedback) {
+		let passwordValid;
+	let minMaxLength = /^[\s\S]{8,32}$/,
+		upper = /[A-Z]/,
+		lower = /[a-z]/,
+		number = /[0-9]/,
+		special = /[ !"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
+
+	if (minMaxLength.test(passwordValue) &&
+		upper.test(passwordValue) &&
+		lower.test(passwordValue) &&
+		number.test(passwordValue) &&
+		special.test(passwordValue)
+	) {
+		passwordFeedback.setAttribute('class', 'hidden');
+		passwordValid = true;
+	} else {
+		passwordFeedback.classList.remove('hidden');
+        passwordValid = false;
+
+	}
+
+    return passwordValid;
+}
+}());
---
+++ register.php
@@ -0,0 +1,75 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Create connection
+    $conn = mysqli_connect("localhost", "jeremykr_twin_cities_forum", "psuh8WIRD.cih8coch", "jeremykr_twin_cities_forum");
+    
+    //We use htmlspecialchars to prevent HTML injection
+    $errors   = "";
+    $username = htmlspecialchars($_POST["username"]);
+    $email    = htmlspecialchars($_POST["email"]);
+    $password = htmlspecialchars($_POST["password"]);
+    
+    if (empty($username) or empty($email) or empty($password) or !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors = "Invalid inputs!";
+        
+    }
+    
+    else {
+        
+        $query = mysqli_query($conn, "SELECT username, email from register WHERE username='$username';");
+        $data  = mysqli_fetch_assoc($query);
+        
+        if (!is_null($data["username"])) {
+            $errors = "Username already exists!";
+        } elseif (!is_null($data["email"])) {
+            $errors = "Email already exists!";
+        } else {
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $query = mysqli_query($conn, "INSERT INTO register (username, email, password) VALUES ('$username', '$email', '$password')");
+            
+            if ($query) {
+                echo "USER created!";
+            } else {
+                echo "It's not working";
+            }
+            
+        }
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<title>Registration</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="preload" as="style" href="style/screen.css" />
+		<link rel="stylesheet" type="text/css" href="style/screen.css" />
+		<script defer src="js/register.js"></script>
+
+	</head>
+
+	<body>
+		<form name="registrationForm" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+			<div class="innerFormOutline">
+				<h1>Registration Page</h1>
+				<p><?php echo $errors; ?></p>
+				<input type="text" id="username" name="username" placeholder="username">
+				<p id="usernameFeedback" class="hidden">Username must have length between 5 and 20 characters. It can
+					may
+					only consist of alphanumeric characters, -, or _.</p>
+				<input type="text" id="email" name="email" placeholder="E-mail">
+				<p id="emailFeedback" class="hidden">Invalid email.</p>
+				<input type="password" id="password" name="password" placeholder="Password" autocomplete="on">
+				<p id="passwordFeedback" class="hidden">Password must contain one lowercase letter, one uppercase
+					letter, one digit, one special character, and is at least eight characters long.</p>
+				<input type="submit" value="Submit" id="submit" class="disabledSubmit">
+			</div>
+		</form>
+	</body>
+
+</html>
